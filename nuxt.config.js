873ae4234478b7449edc859a7abdccbf6db2a660
zhangyan36 <zhangyan36@58ganji.com>
2020-12-12 00:56:50 +0800

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,6 +45,7 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    publicPath: `https://static.yladm.com/Transformers/Bruticus/prods/${version}/dist/client/`,
     filenames: {
       css: ({ isDev }) => (isDev ? "[name].js" : "[name].[contenthash:7].css"),
       app: ({ isDev }) => (isDev ? "[name].js" : "[name].[contenthash:7].js"),
