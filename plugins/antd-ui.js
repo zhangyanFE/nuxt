f39a2e2c5d3f69bbee7c5d6f2a0e166b44b6f3f0
zhangyan36 <zhangyan36@58ganji.com>
2020-12-13 12:10:37 +0800

    update

--- plugins/antd-ui.js
+++ plugins/antd-ui.js
@@ -1,6 +1,16 @@
+import { version } from "../package.json";
 import Vue from "vue";
 import { Layout, Menu, Row, Col } from "ant-design-vue";
 const { Item } = Menu;
 
 const { Header, Footer, Content } = Layout;
-Vue.use(Layout).use(Header).use(Footer).use(Content).use(Menu).use(Item).use(Row).use(Col);
+Vue.use(Layout)
+  .use(Header)
+  .use(Footer)
+  .use(Content)
+  .use(Menu)
+  .use(Item)
+  .use(Row)
+  .use(Col);
+
+console.log(`production:version(${version})`);
