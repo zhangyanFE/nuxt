85b956507dd846bed2bdedca5505cc0dc248310e
zhangyan36 <zhangyan36@58ganji.com>
2020-12-15 11:55:14 +0800

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,25 +15,25 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  generate: {
-    routes: [
-      '/newsdetail/0',
-      '/newsdetail/1',
-      '/newsdetail/2',
-      '/newsdetail/3',
-      '/newsdetail/4',
-      '/newsdetail/5',
-      '/newsdetail/6',
-      '/newsdetail/7',
-      '/newsdetail/8',
-      '/productdetail/0',
-      '/productdetail/1',
-      '/productdetail/2',
-      '/productdetail/3',
-      '/productdetail/4',
-      '/productdetail/5',
-    ]
-  },
+  // generate: {
+  //   routes: [
+  //     '/newsdetail/0',
+  //     '/newsdetail/1',
+  //     '/newsdetail/2',
+  //     '/newsdetail/3',
+  //     '/newsdetail/4',
+  //     '/newsdetail/5',
+  //     '/newsdetail/6',
+  //     '/newsdetail/7',
+  //     '/newsdetail/8',
+  //     '/productdetail/0',
+  //     '/productdetail/1',
+  //     '/productdetail/2',
+  //     '/productdetail/3',
+  //     '/productdetail/4',
+  //     '/productdetail/5',
+  //   ]
+  // },
   router: {
     trailingSlash: false
   },
